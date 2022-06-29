e2fb12c6dad6d76664265348039d7ebd8a59041f
Cedric Davis <101026754+TaronDavisJr@users.noreply.github.com>
2022-06-29 12:13:56 -0700

    Add files via upload

---
+++ Bank of the East ( Java ) /src/BankoftheEast.java
@@ -0,0 +1,17 @@
+package src;
+public class BankoftheEast {
+
+	public static void main(String[] args) {
+	
+
+	Account ced = new Account("Cedric Davis", "639914");
+		ced.showMenu();
+
+		
+		
+		Account Jamal = new Account("Jamal", "B00002");
+		Jamal.showMenu();
+		
+	}
+
+}
